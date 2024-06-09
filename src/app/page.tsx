d1602c262f447653065506b2e93a0e497ab5b3ad
soma <swfh2367@gmail.com>
2024-06-09 23:35:06 +0900

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://picsum.photos/id/32/900/600.jpg",
@@ -13,12 +13,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <Image
               src={image.url}
               alt={`image-${image.id}`}
