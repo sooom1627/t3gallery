6c55df247a1f0a961bac03ab50cc834e13e09dd9
soma <swfh2367@gmail.com>
2024-06-09 23:01:40 +0900

    sccaford UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,14 +8,26 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex items-center justify-between border-b p-4 ">
+      <div className="tex-xl font-semibold">Gallery</div>
+      <div>Sign in</div>
+    </nav>
+  );
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={`${GeistSans.variable} flex flex-col gap-4 font-sans`}>
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+import Image from "next/image";
 import Link from "next/link";
+
+const mockUrls = [
+  "https://picsum.photos/id/32/900/600.jpg",
+  "https://picsum.photos/id/48/900/600.jpg",
+  "https://picsum.photos/id/64/900/600.jpg",
+  "https://picsum.photos/id/80/900/600.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <Image
+              src={image.url}
+              alt={`image-${image.id}`}
+              width={300}
+              height={200}
+            />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
